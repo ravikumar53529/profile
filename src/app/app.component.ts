9715ca40339e54eb8e53ff9b0c2ebbd95aa4cfb2
ravikumarg <galinki.ravi333@gmail.com>
2025-05-10 15:28:20 +0530

    portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,4 +18,11 @@ export class AppComponent {
   // onRemove(index:number){
   //    this.array.splice(index,1)
   // }
+
+  downloadResume(): void {
+  const link = document.createElement('a');
+  link.href = 'assets/Ravikumar_G.pdf';
+  link.download = 'Ravikumar_G.pdf'; // Optional: change the name users will see
+  link.click();
+}
 }
